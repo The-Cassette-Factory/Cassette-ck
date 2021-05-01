0a2bea1c6f6468e26b8e9d37b4eea3b6733db7fb
The Cassette <sahusk.13oct@gmail.com>
2021-05-01 18:09:44 +0530

    Update cloudkitchen.java

--- cloudkitchen.java
+++ cloudkitchen.java
@@ -1,4 +1,4 @@
 its the solution for all restaurent problems.
-
+the cloud kitchen can be possible by link it via cloud platforms like zomato,swiggy, speed zy.
 
 cloud kitchen is based on cloud concept
